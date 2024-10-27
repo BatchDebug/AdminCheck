a2ebb4da5b23b4f1edf4bb18eb02cf279bc69df7
BatchDebug <dieeinzigenkisten@gmail.com>
2024-10-27 11:53:01 +0100

    Add files via upload

---
+++ AdminCheck.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+title Admin Check by BatchDebug
+set "vbsFile=%temp%\AdminCheckErrorMsg.vbs"
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    net session >nul 2>&1
+    exit /b
+)
+title Example Title
+echo Running as an Administrator.
+:: Place any additional commands here
+pause
+exit
